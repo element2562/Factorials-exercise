1f415f4333bb03440e350816cd636afbd7467a73
element2562 <element25668@gmail.com>
2018-09-30 09:41:35 -0500

    Finished factorial exercise

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class MainClass
+{
+    public static int FirstFactorial(int num)
+    {
+
+        // code goes here  
+        /* Note: In C# the return type of a function and the 
+           parameter types being passed are defined, so this return 
+           call must match the return type of the function.
+           You are free to modify the return type. */
+        int factorial = 1;
+        for (int i = 1; i <= num; i++)
+        {
+            factorial = factorial * i;
+        }
+        return factorial;
+
+    }
+
+    static void Main()
+    {
+        // keep this function call here
+        Console.WriteLine(FirstFactorial(8));
+    }
+
+}
+
